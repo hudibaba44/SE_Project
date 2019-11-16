24bb963b74c9ca5ad72c4bca3d899c6f3841227c
pallavim98 <pallavim98@gmail.com>
2019-11-16 09:17:55 +0530

    temp_backend

---
+++ backend/backend.py
@@ -0,0 +1,81 @@
+import pymongo
+from flask import Flask, jsonify, abort, make_response, request, url_for, session
+from flask import render_template, redirect
+import json
+import re
+import os
+import hashlib
+
+app = Flask(__name__)
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+db = myclient["database"]
+users = db["users"]
+frameworkdb = db["framework"]
+
+def cleardb():
+    db.users.drop()
+
+@app.route("/", methods = ["GET"])
+def test():
+    cleardb()
+    return jsonify({}),200
+
+@app.route("/signup", methods = ["POST"])
+def add_user():
+    req = eval(request.data)
+    name = req["name"]
+    user_email = req["email_id"]
+    pwd = req["password"]
+    if users.find_one({user_email : { '$exists' : True } } ) == None:
+        document = {user_email : pwd, "name" : name}
+        x = users.insert_one(document)
+        status = 200
+        print(x)
+    else:
+        x = users.find_one({user_email : { '$exists' : True } } )
+        print(x)
+        status = 400
+    return jsonify({}), status
+
+@app.route("/login", methods = ["POST"])
+def login():
+    req = eval(request.data)
+    user_email = req["email_id"]
+    pwd = req["password"]
+    x = users.find_one({user_email : { '$exists' : True } } )
+    if x == None:
+        status = 404
+    elif x[user_email] == pwd:
+        session['user'] = user_email
+        status = 200
+    else:
+        status = 403
+    return jsonify(session['user']), status
+
+@app.route("/logout", methods = ["GET"])
+def logout():
+    session.pop('username', None)
+    return jsonify({}),200
+
+@app.route("/framework_signup", methods = ["POST"])
+def framework_signup():
+    req = eval(request.data)
+    email_id = req["email_id"]
+    path = req["path"]
+    framework = req["framework"]
+    folder_path = os.path.join(oath, framework)
+    os.mkdir(folder_path,0777)
+    document = {id : email_id, framework_path : folder_path}
+    x = frameworkdb.find_one(document)
+    if x == None:
+        frameworkdb.insert_one(x)
+        # create git repo
+        status = 200
+    else:
+        status = 400
+    return jsonify({}), status
+
+
+if __name__ == '__main__':
+    app.secret_key = "secret_key"
+    app.run(host="127.0.0.1" ,port=80)

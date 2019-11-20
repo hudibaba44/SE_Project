d8e73f3be520079799db37e98d15d6523b1f5905
Neelesh C A <neeleshca26@gmail.com>
2019-11-20 16:30:57 +0530

    git auth token

--- backend/git_backend.py
+++ backend/git_backend.py
@@ -19,3 +19,30 @@ class git_service:
         req_api = "/admin/users/{}/repos".format(email_id)
         resp = requests.post(self.drone_ip+req_api, data);
         return resp
+
+data = {
+  "email": "user@example.com",
+  "full_name": "string",
+  "login_name": "string",
+  "must_change_password": True,
+  "password": "string",
+  "send_notify": True,
+  "source_id": 0,
+  "username": "string"
+}
+
+data = {
+  "auto_init": True,
+  "description": "string",
+  "issue_labels": "string",
+  "name": "string",
+  "private": True
+}
+
+params = (
+    ('access_token', '95a957a9ca86b0ee4edcdf3e4f21b101e289e30a'),
+)
+
+# headers = {"Authorization": "token " + "95a957a9ca86b0ee4edcdf3e4f21b101e289e30a"}
+# requests.post("http://192.168.43.109:3000/api/v1/admin/users", json = data, params=params)
+requests.post("http://192.168.43.109:3000/api/v1/admin/users/string/repos", json = data, params=params)
